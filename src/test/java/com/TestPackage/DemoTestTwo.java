ba7c55f9d4752b6b89c391c233db6840ef7f301d
Sam Mosawi <sam@Sams-MBP.fios-router.home>
2020-04-01 13:57:17 -0400

    heeey

--- src/test/java/com/TestPackage/DemoTestTwo.java
+++ src/test/java/com/TestPackage/DemoTestTwo.java
@@ -1,5 +1,44 @@
 package com.TestPackage;
 
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
 public class DemoTestTwo {
+	@Test
+	public void  sum() {
+		System.out.println("sum Class 22222");
+		int a = 10;
+		int b = 30;
+		int x;
+		Assert.assertEquals(40, a+b);
+	}
+	
+	@Test
+	public void  sub() {
+		System.out.println("subtract");
+
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(20, b-a);
+	}
+	@Test
+	public void  div() {
+		System.out.println("division");
+
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(3, b/a);
+	}
+	
+	@Test
+	public void  multi() {
+		System.out.println("multiply");
+
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(300, a*b);
+	}
+
 
 }
--- src/test/java/com/TestPackage/TestDemoOne.java
+++ src/test/java/com/TestPackage/TestDemoOne.java
@@ -1,5 +1,43 @@
 package com.TestPackage;
 
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
 public class TestDemoOne {
+	@Test
+	public void  sum() {
+		System.out.println("sum class One ");
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(40, a+b);
+	}
+	
+	@Test
+	public void  sub() {
+		System.out.println("subtract");
+
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(20, b-a);
+	}
+	@Test
+	public void  div() {
+		System.out.println("division");
+
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(3, b/a);
+	}
+	
+	@Test
+	public void  multi() {
+		System.out.println("multiply");
+
+		int a = 10;
+		int b = 30;
+		Assert.assertEquals(300, a*b);
+	}
+
 
 }
